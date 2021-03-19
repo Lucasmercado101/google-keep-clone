6e342226ffa3f460572b6a3c519b80d54ae369e2
lucasmercado101 <lucasmercado101@gmail.com>
2021-03-19 10:33:24 -0300

    fixed api

--- front-end/src/api.ts
+++ front-end/src/api.ts
@@ -4,6 +4,12 @@ axios.defaults.baseURL = "http://localhost:5000";
 
 export const logIn = (data: { userName: string; password: string }) => {
   return axios
-    .post<{ userName: string }>("/auth/login", data)
+    .post<{ userName: string }>("/auth/login", data, { withCredentials: true })
+    .then((resp) => resp.data);
+};
+
+export const isLoggedIn = () => {
+  return axios
+    .get<{ userName: string }>("/auth/me", { withCredentials: true })
     .then((resp) => resp.data);
 };

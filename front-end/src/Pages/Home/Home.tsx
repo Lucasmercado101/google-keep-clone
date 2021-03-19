5977dc4806310ab33fc225bdf2d973b6c099e025
lucasmercado101 <lucasmercado101@gmail.com>
2021-03-19 13:40:59 -0300

    added loading placeholder in home

--- front-end/src/Pages/Home/Home.tsx
+++ front-end/src/Pages/Home/Home.tsx
@@ -1,19 +1,24 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { RouteChildrenProps } from "react-router";
 import { GlobalStateContext } from "../../StateProvider";
 import { isLoggedIn } from "../../api";
 import NavBar from "../../Components/NavBar/NavBar";
 
 const Home: React.FC<RouteChildrenProps> = ({ history }) => {
+  const [isLoading, setIsLoading] = useState(true);
   let ctx = useContext(GlobalStateContext);
   //TODO: notifications on the notes
+
   useEffect(() => {
     isLoggedIn()
       .then((data) => {
+        setIsLoading(false);
         ctx.userData = data;
       })
       .catch(() => history.replace("/"));
   }, []);
+
+  if (isLoading) return null;
 
   return (
     <div>

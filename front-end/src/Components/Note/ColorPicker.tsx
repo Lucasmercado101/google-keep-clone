5e7ef750ba5fb55bf9ca2158df4814d903fd4fcd
lucasmercado101 <lucasmercado101@gmail.com>
2021-03-21 14:18:10 -0300

    borders on colors on hover

--- front-end/src/Components/Note/ColorPicker.tsx
+++ front-end/src/Components/Note/ColorPicker.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { ColorLensOutlined as CanvasIcon } from "@material-ui/icons";
 import { IconButton, makeStyles, Popover, Paper } from "@material-ui/core";
+import clsx from "clsx";
 
 const useStyles = makeStyles((theme) => ({
   iconContainer: {
@@ -13,9 +14,40 @@ const useStyles = makeStyles((theme) => ({
   },
   paper: {
     padding: theme.spacing(1),
-    pointerEvents: "auto"
+    pointerEvents: "auto",
+    display: "grid",
+    gridTemplateColumns: "26px 26px 26px 26px",
+    gridTemplateRows: "26px 26px 26px",
+    gap: 5
   },
-  popover: { pointerEvents: "none" }
+  popover: { pointerEvents: "none" },
+  color: {
+    minWidth: 26,
+    maxWidth: 26,
+    minHeight: 26,
+    maxHeight: 26,
+    borderRadius: "50%",
+    opacity: 0.3,
+    position: "relative"
+  },
+  colorWrapper: {
+    cursor: "pointer",
+    position: "relative",
+    "&:hover $colorSelectedOutline": {
+      border: `2px solid ${theme.palette.text.primary}`
+    }
+  },
+  colorSelectedOutline: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    borderRadius: "50%"
+  },
+  transparentColor: {
+    border: `2px solid ${theme.palette.text.primary}`
+  }
 }));
 
 type props = {
@@ -35,10 +67,13 @@ const ColorPicker: React.FC<props> = ({ onSelectColor }) => {
   const handlePopoverClose = () => {
     setAnchorEl(null);
   };
-
+  // 26 * 26 y 4 width * 3 height
+  //   default ,red, orange yellow
+  // green, Teal, Blue, Dark Blue,
+  // Purple ,Pink , Brown, Gray
   const open = Boolean(anchorEl);
   return (
-    <div onMouseLeave={handlePopoverClose}>
+    <div onClick={(e) => e.stopPropagation()} onMouseLeave={handlePopoverClose}>
       <IconButton
         onMouseEnter={handlePopoverOpen}
         className={classes.iconContainer}
@@ -61,7 +96,61 @@ const ColorPicker: React.FC<props> = ({ onSelectColor }) => {
         }}
         onClose={handlePopoverClose}
       >
-        <Paper className={classes.paper}>I use Popover.</Paper>
+        <Paper className={classes.paper}>
+          <div className={classes.colorWrapper}>
+            <div
+              style={{ background: "transparent" }}
+              className={clsx(classes.color, classes.transparentColor)}
+            />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "red" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "orange" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "yellow" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "green" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "teal" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "blue" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "darkblue" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "purple" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "pink" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "brown" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+          <div className={classes.colorWrapper}>
+            <div style={{ background: "gray" }} className={classes.color} />
+            <div className={classes.colorSelectedOutline} />
+          </div>
+        </Paper>
       </Popover>
     </div>
   );

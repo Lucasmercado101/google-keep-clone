61dc7611fc61ecea8194d296fdfd64b1117a0c7d
lucasmercado101 <lucasmercado101@gmail.com>
2021-03-27 17:18:04 -0300

    can add new label from edit labels menu

--- front-end/src/Components/EditLabelsMenu/EditLabelsMenu.tsx
+++ front-end/src/Components/EditLabelsMenu/EditLabelsMenu.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import {
   makeStyles,
   Paper,
@@ -13,12 +13,19 @@ import {
   InputBase,
   Modal
 } from "@material-ui/core";
-import { useDeleteLabel, useGetLabels, usePutLabel } from "../../Hooks/queries";
+import {
+  useDeleteLabel,
+  useGetLabels,
+  usePostNewLabel,
+  usePutLabel
+} from "../../Hooks/queries";
 import {
   Label as LabelIcon,
   Delete as TrashIcon,
   Edit as EditIcon,
-  Check as ConfirmIcon
+  Check as ConfirmIcon,
+  Add as AddIcon,
+  Close as CancelIcon
 } from "@material-ui/icons";
 import clsx from "clsx";
 
@@ -101,6 +108,13 @@ const useStyles = makeStyles((theme) => ({
     display: "flex",
     gap: 15,
     justifyContent: "flex-end"
+  },
+  addNewLabelContainer: {
+    listStyleType: "none"
+  },
+  textInputFocused: {
+    borderBottom: `thin solid ${theme.palette.text.secondary}`,
+    marginBottom: -1
   }
 }));
 
@@ -114,9 +128,13 @@ const EditLabelsMenu: React.FC<props> = ({ onClose }) => {
   const [isEditing, setIsEditing] = useState<null | number>();
   const deleteLabel = useDeleteLabel();
   const putLabel = usePutLabel();
+  const postNewLabel = usePostNewLabel();
   const [labelToDeleteId, setLabelToDeleteId] = useState<null | number>();
   const [isDeleteLabelModalOpen, setIsDeleteLabelModalOpen] = useState(false);
   const [newLabelsData, setNewLabelsData] = useState<{} | any>({});
+  const [newLabelName, setNewLabelName] = useState("");
+  const [isWritingNewName, setIsWritingNewName] = useState(false);
+  const newLabelInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     let newLabels: any = {};
@@ -129,6 +147,52 @@ const EditLabelsMenu: React.FC<props> = ({ onClose }) => {
       <Paper className={classes.container}>
         <div className={classes.labelsList}>
           <Typography className={classes.title}>Edit labels</Typography>
+
+          <ListItem classes={{ container: classes.addNewLabelContainer }}>
+            <ListItemIcon className={classes.listItemIcon}>
+              <IconButton color="inherit" size="small">
+                {isWritingNewName ? (
+                  <CancelIcon />
+                ) : (
+                  <AddIcon
+                    onClick={() => {
+                      setNewLabelName("");
+                      newLabelInputRef?.current?.focus();
+                    }}
+                  />
+                )}
+              </IconButton>
+            </ListItemIcon>
+
+            <InputBase
+              placeholder="Create new label"
+              inputProps={{
+                className: classes.listItemText,
+                onFocus: () => setIsWritingNewName(true),
+                ref: newLabelInputRef
+              }}
+              value={newLabelName}
+              onChange={(e) => setNewLabelName(e.target.value)}
+            />
+            <ListItemSecondaryAction>
+              {isWritingNewName && (
+                <IconButton
+                  onClick={() => {
+                    if (newLabelName) {
+                      postNewLabel(newLabelName);
+                      setNewLabelName("");
+                      setIsWritingNewName(false);
+                    }
+                  }}
+                  className={classes.listItemIcon}
+                  size="small"
+                >
+                  <ConfirmIcon />
+                </IconButton>
+              )}
+            </ListItemSecondaryAction>
+          </ListItem>
+
           {labelsData && (
             <List>
               {labelsData.map((label) => (
@@ -162,7 +226,10 @@ const EditLabelsMenu: React.FC<props> = ({ onClose }) => {
                   </ListItemIcon>
 
                   <InputBase
-                    onFocus={() => setIsEditing(label.id)}
+                    onFocus={() => {
+                      setIsEditing(label.id);
+                      setIsWritingNewName(false);
+                    }}
                     inputProps={{ className: classes.listItemText }}
                     value={newLabelsData[label.id]}
                     onChange={(e) =>

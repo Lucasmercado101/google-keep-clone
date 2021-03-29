a6ac106354fcef4586e6f27f30d806bad44d86d7
lucasmercado101 <lucasmercado101@gmail.com>
2021-03-29 08:22:12 -0300

    progress

--- server/tests/auth.test.ts
+++ server/tests/auth.test.ts
@@ -18,48 +18,71 @@ describe("/auth", () => {
   describe("/register", () => {
     describe("POST", () => {
       describe("Should return ", () => {
-        it("400 if nothing was sent", (done) =>
-          app
-            .post("/auth/register")
-            .expect(400)
-            .then((resp) => {
-              expect(resp.body).toBe(
-                "You need to provide both a 'userName' and 'password'"
-              );
-              done();
-            }));
+        describe("400 if", () => {
+          it("nothing was sent", (done) =>
+            app
+              .post("/auth/register")
+              .send({ password: "superSafePasswordISwear" })
+              .expect(400)
+              .end(done));
 
-        it("400 if only 'userName' was sent", (done) =>
-          app
-            .post("/auth/register")
-            .send({ userName: "john" })
-            .expect(400)
-            .then((resp) => {
-              expect(resp.body).toBe(
-                "You need to provide both a 'userName' and 'password'"
-              );
-              done();
-            }));
+          it("only 'userName' was sent", (done) =>
+            app
+              .post("/auth/register")
+              .send({ password: "superSafePasswordISwear" })
+              .expect(400)
+              .end(done));
 
-        it("400 if only 'password' was sent", (done) =>
-          app
-            .post("/auth/register")
-            .send({ password: "superSafePasswordISwear" })
-            .expect(400)
-            .then((resp) => {
-              expect(resp.body).toBe(
-                "You need to provide both a 'userName' and 'password'"
-              );
-              done();
-            }));
+          it("only 'password' was sent", (done) =>
+            app
+              .post("/auth/register")
+              .send({ password: "superSafePasswordISwear" })
+              .expect(400)
+              .end(done));
+        });
 
-        describe("if both 'userName' and 'password' were sent ", () => {
+        describe("an error message if", () => {
+          it("nothing was sent", (done) =>
+            app
+              .post("/auth/register")
+              .expect(400)
+              .then((resp) => {
+                expect(resp.body).toBe(
+                  "You need to provide both a 'userName' and 'password'"
+                );
+                done();
+              }));
+          it("only 'password' was sent", (done) =>
+            app
+              .post("/auth/register")
+              .send({ password: "superSafePasswordISwear" })
+              .expect(400)
+              .then((resp) => {
+                expect(resp.body).toBe(
+                  "You need to provide both a 'userName' and 'password'"
+                );
+                done();
+              }));
+          it("only 'userName' was sent", (done) =>
+            app
+              .post("/auth/register")
+              .send({ userName: "john" })
+              .expect(400)
+              .then((resp) => {
+                expect(resp.body).toBe(
+                  "You need to provide both a 'userName' and 'password'"
+                );
+                done();
+              }));
+        });
+
+        describe("if both 'userName' and 'password' were sent", () => {
           beforeEach(async () => await User.sync({ force: true }));
 
-          it("it should give a status 200", (done) =>
+          it("200", (done) =>
             app.post("/auth/register").send(newUser).expect(200).end(done));
 
-          it("it should return an object that contains the user's userName and encrypted password", (done) =>
+          it("an object that contains the user's userName and encrypted password", (done) =>
             app
               .post("/auth/register")
               .send(newUser)
@@ -82,10 +105,10 @@ describe("/auth", () => {
             await User.create({ userName: newUser.userName });
           });
 
-          it("should return a 409", (done) => {
+          it("409", (done) => {
             app.post("/auth/register").send(newUser).expect(409).end(done);
           });
-          it("should return an error message", (done) => {
+          it("an error message", (done) => {
             app
               .post("/auth/register")
               .send(newUser)

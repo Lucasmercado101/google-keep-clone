895844ffa20057e55771622dac2df119e3e66d69
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-05 12:33:17 -0300

    fixed DeleteNote.ts

--- server/dev/routes/notes/id/deleteNote.ts
+++ server/dev/routes/notes/id/deleteNote.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import Note from "../../../db/models/Note";
 import isAuthenticated from "../../middleware/isAuthenticated";
+import isParamANumber from "../../middleware/isParamANumber";
 
 const ROUTE = "/notes/:noteId";
 
 export default Router({ mergeParams: true }).delete(
   ROUTE,
   isAuthenticated,
+  isParamANumber("noteId"),
   async (req, res) => {
     const { noteId } = req.params;
-    await Note.destroy({ where: { id: noteId, author: req.user!.userName } });
+    const note = await Note.findByPk(noteId);
+    if (!note) return res.sendStatus(404);
+    await note.destroy();
     res.sendStatus(200);
   }
 );

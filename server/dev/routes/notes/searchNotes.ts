3ebdd74a97d848d80de06315b1133aa2dbe5de63
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-10 09:39:59 -0300

    can search by labels

--- server/dev/routes/notes/searchNotes.ts
+++ server/dev/routes/notes/searchNotes.ts
@@ -2,33 +2,50 @@ import { Router } from "express";
 import Note from "../../db/models/Note";
 import { Op } from "sequelize";
 import isAuthenticated from "../middleware/isAuthenticated";
+import Label from "../../db/models/Label";
 
 const ROUTE = "/notes/search";
-
-const noteAttributesToReturn = [
-  "title",
-  "content",
-  "id",
-  "pinned",
-  "archived",
-  "color"
-];
 
 export default Router({ mergeParams: true }).get(
   ROUTE,
   isAuthenticated,
   async (req, res) => {
-    const { searchTerm, limit = 30 } = req.query;
+    const { query, labels } = req.body;
 
-    await Note.findAll({
-      where: {
-        [Op.or]: {
-          title: { [Op.iLike]: `%${searchTerm}%` },
-          content: { [Op.iLike]: `%${searchTerm}%` }
-        }
-      },
-      limit: +limit,
-      attributes: noteAttributesToReturn
-    }).then((notes) => res.json(notes));
+    if (labels) {
+      const iLikes = (labels as string[]).map((labelName) => ({
+        [Op.iLike]: `%${labelName}%`
+      }));
+
+      await req
+        .user!.$get("notes", {
+          order: [["id", "ASC"]],
+          include: [
+            {
+              model: Label,
+              through: { attributes: [] },
+              where: {
+                name: {
+                  [Op.or]: iLikes
+                }
+              }
+            }
+          ]
+        })
+        .then((notes) => res.json(notes));
+    } else {
+      req
+        .user!.$get("notes", {
+          order: [["id", "ASC"]],
+          where: {
+            [Op.or]: {
+              title: { [Op.iLike]: `%${query}%` },
+              content: { [Op.iLike]: `%${query}%` }
+            }
+          },
+          include: [{ model: Label, through: { attributes: [] } }]
+        })
+        .then((notes) => res.json(notes));
+    }
   }
 );

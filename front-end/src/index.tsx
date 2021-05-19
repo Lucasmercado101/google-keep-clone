7d9238475530227f2cd36c9f7cab16a9805fd2dc
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-19 20:21:11 -0300

    added router providers

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -2,32 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router } from "react-router-dom";
 import { CssBaseline } from "@material-ui/core";
 import ThemeProvider from "./ThemeProvider";
 import GlobalStateProvider from "./StateProvider";
 import { QueryClientProvider, QueryClient } from "react-query";
+import { RouterProvider } from "react-router5";
+import createRouter from "./Router";
+import FSMProvider from "./RouterContext";
+
+const router = createRouter();
 
 const client = new QueryClient();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <GlobalStateProvider>
-        <QueryClientProvider client={client}>
-          <ThemeProvider>
-            <App />
-            <CssBaseline />
-          </ThemeProvider>
-        </QueryClientProvider>
-      </GlobalStateProvider>
-    </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+router.start(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <FSMProvider>
+        <RouterProvider router={router}>
+          <GlobalStateProvider>
+            <QueryClientProvider client={client}>
+              <ThemeProvider>
+                <App />
+                <CssBaseline />
+              </ThemeProvider>
+            </QueryClientProvider>
+          </GlobalStateProvider>
+        </RouterProvider>
+      </FSMProvider>
+    </React.StrictMode>,
+    document.getElementById("root")
+  );
+});

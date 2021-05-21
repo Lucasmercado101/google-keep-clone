e0d7077180d3374e3d2b33d6c6e008ba2ea75dd4
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-21 09:39:15 -0300

    added more states

--- front-end/src/Components/Note/noteMachine.ts
+++ front-end/src/Components/Note/noteMachine.ts
@@ -1,20 +1,26 @@
 import { Machine, assign } from "xstate";
 
 export enum stateTypes {
-  MOUSE_HOVERED_ON = "MOUSE_HOVERED_ON"
+  MOUSE_HOVERED_ON = "MOUSE_HOVERED_ON",
+  IDLE = "IDLE"
 }
 export enum sendTypes {
-  MOUSE_HOVERED_ON_NOTE = "MOUSE_HOVERED_ON_NOTE"
+  MOUSE_HOVERED_ON_NOTE = "MOUSE_HOVERED_ON_NOTE",
+  MOUSE_LEFT_NOTE_AREA = "MOUSE_LEFT_NOTE_AREA"
 }
 
 export const noteMachine = Machine({
-  initial: "idle",
+  initial: stateTypes.IDLE,
   states: {
-    idle: {
+    [stateTypes.IDLE]: {
       on: {
         [sendTypes.MOUSE_HOVERED_ON_NOTE]: stateTypes.MOUSE_HOVERED_ON
       }
     },
-    [stateTypes.MOUSE_HOVERED_ON]: {}
+    [stateTypes.MOUSE_HOVERED_ON]: {
+      on: {
+        [sendTypes.MOUSE_LEFT_NOTE_AREA]: stateTypes.IDLE
+      }
+    }
   }
 });
